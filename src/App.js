cd5fd13c9fcc229fab790f5b8467001b07d81016
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-04-27 19:38:17 -0300

    Atende aos requisitos 13 ao 18.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import AppProvider from './context/AppProvider';
 import DoneRecipes from './pages/DoneRecipes/DoneRecipes';
+import DrinkDetails from './pages/DrinkDetails/DrinkDetails';
 import Drinks from './pages/Drinks/Drinks';
 import Explore from './pages/Explore/Explore';
 import ExploreDrinks from './pages/ExploreDrinks/ExploreDrinks';
@@ -12,6 +13,7 @@ import ExploreFoods from './pages/ExploreFoods/ExploreFoods';
 import FoodsIngredients from './pages/ExploreFoodsIngredients/FoodsIngredients';
 import FoodsNationalities from './pages/ExploreFoodsNationalities/FoodsNationalities';
 import FavoriteRecipes from './pages/FavoriteRecipes/FavoriteRecipes';
+import FoodDetails from './pages/FoodDetails/FoodDetails';
 import Foods from './pages/Foods/Foods';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
@@ -25,6 +27,8 @@ function App() {
         <Route exact path="/explore" component={ Explore } />
         <Route exact path="/done-recipes" component={ DoneRecipes } />
         <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route path="/foods/:id" component={ FoodDetails } />
+        <Route path="/drinks/:id" component={ DrinkDetails } />
         <Route exact path="/explore/foods" component={ ExploreFoods } />
         <Route exact path="/explore/drinks" component={ ExploreDrinks } />
         <Route exact path="/explore/foods/ingredients" component={ FoodsIngredients } />
