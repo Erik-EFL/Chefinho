ca16b457fc64cddbbfa30ead24e9d2e4a464719c
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-04-28 19:51:37 -0300

    Correção rodapé

--- src/pages/FavoriteRecipes/FavoriteRecipes.js
+++ src/pages/FavoriteRecipes/FavoriteRecipes.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../../Components/Header';
-import MenuInferior from '../../Components/MenuInferior';
 
 export default function FavoriteRecipes() {
   return (
@@ -9,7 +8,6 @@ export default function FavoriteRecipes() {
         title="Favorite Recipes"
         searchButton={ false }
       />
-      <MenuInferior />
     </div>
   );
 }
