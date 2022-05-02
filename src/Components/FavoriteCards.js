6d3f3a9cd3e57347f39f817ce60f714da4541ec7
Luiz Guilherme <guilhermerx18@gmail.com>
2022-05-02 13:15:24 -0300

    requisito 60 em andamento

--- src/Components/FavoriteCards.js
+++ src/Components/FavoriteCards.js
@@ -1,11 +1,45 @@
-import PropTypes from 'prop-types';
+import clipboardCopy from 'clipboard-copy';
+import PropTypes, { object } from 'prop-types';
 import React from 'react';
+import blackHeart from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+import { setFavoriteDrink, setFavoriteFood } from '../Service/setFavorite';
 
-export default function FavoriteCards({ image, title }) {
+export default function FavoriteCards({ image, title, index }) {
+  const [copy, setCopy] = useState(false);
+
+  const removeFavorite = () => {
+    if (object.type === 'food') return setFavoriteFood(object, () => {});
+    if (object.type === 'drink') return setFavoriteDrink(object, () => {});
+  };
+
+  const handleShareButton = () => {
+    clipboardCopy(`http://localhost:3000/foods/${id}`);
+    setCopy(true);
+  };
   return (
     <div>
-      <img src={ image } alt={ title } />
-      <h2>{title}</h2>
+      <img src={ image } alt={ title } data-testid={ `${index}-horizontal-image` } />
+      <p
+        data-testid={ `${index}-horizontal-top-text` }
+      >
+        {`${nationality} - ${category}`}
+      </p>
+      <h2 data-testid={ `${index}-horizontal-name` }>{title}</h2>
+      <button
+        type="button"
+        onClick={ handleShareButton }
+      >
+        <img
+          src={ shareIcon }
+          alt="ShareBtn"
+          data-testid={ `${index}-horizontal-share-btn` }
+        />
+      </button>
+      {copy && <p>Link copied!</p>}
+      <button type="button" onClick={ () => removeFavorite() }>
+        <img src={ blackHeart } alt="blackHeart" />
+      </button>
     </div>
   );
 }
