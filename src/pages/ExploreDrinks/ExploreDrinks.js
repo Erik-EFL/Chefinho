7f5fa1a0af1ebcb70f1126af92b9522a2562c296
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-05-02 18:38:58 -0300

    Atende requisitos 67 ao 74.

--- src/pages/ExploreDrinks/ExploreDrinks.js
+++ src/pages/ExploreDrinks/ExploreDrinks.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../Components/Header';
 import MenuInferior from '../../Components/MenuInferior';
+import fetchRandomDrink from '../../Service/fetchRandomDrink';
 
 export default function ExploreDrinks() {
+  const history = useHistory();
+
+  const handleSurprise = async () => {
+    const randomDrink = await fetchRandomDrink();
+    history.push(`/drinks/${randomDrink[0].idDrink}`);
+  };
+
   return (
     <div>
       <Header
         title="Explore Drinks"
         searchButton={ false }
       />
+      <div>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explore/drinks/ingredients') }
+        >
+          By Ingredient
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ handleSurprise }
+        >
+          Surprise me!
+        </button>
+      </div>
       <MenuInferior />
     </div>
   );
--- src/pages/ExploreFoods/ExploreFoods.js
+++ src/pages/ExploreFoods/ExploreFoods.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../../Components/Header';
 import MenuInferior from '../../Components/MenuInferior';
+import fetchRandomFood from '../../Service/fetchRandomFood';
 
 export default function ExploreFoods() {
+  const history = useHistory();
+
+  const handleSurprise = async () => {
+    const randomFood = await fetchRandomFood();
+    history.push(`/foods/${randomFood[0].idMeal}`);
+  };
+
   return (
     <div>
       <Header
         title="Explore Foods"
         searchButton={ false }
       />
+      <div>
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+          onClick={ () => history.push('/explore/foods/ingredients') }
+        >
+          By Ingredient
+        </button>
+        <button
+          type="button"
+          data-testid="explore-by-nationality"
+          onClick={ () => history.push('/explore/foods/nationalities') }
+        >
+          By Nationality
+        </button>
+        <button
+          type="button"
+          data-testid="explore-surprise"
+          onClick={ handleSurprise }
+        >
+          Surprise me!
+        </button>
+      </div>
       <MenuInferior />
     </div>
   );
