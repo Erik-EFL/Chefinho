28f6fa1398dfca5e437d5f30649218ef3efee8e5
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-05-03 09:59:32 -0300

    Atende requisitos 78 ao 81.

---
+++ src/Components/NationalitiesDropDown.js
@@ -0,0 +1,93 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import PropTypes from 'prop-types';
+import React, { useContext, useEffect, useState } from 'react';
+import AppContext from '../context/AppContext';
+import fetchDrinks from '../Service/fetchDrinks';
+import fetchFilteredByNationality from '../Service/fetchFilteredByNationality';
+import fetchFoods from '../Service/fetchFoods';
+import fetchNationalities from '../Service/fetchNationalities';
+
+export default function NationalitiesDropDown(props) {
+  const {
+    type,
+  } = props;
+  const [nationalities, setNationalities] = useState([]);
+  const {
+    setFoods,
+    setDrinks,
+    activeFilter,
+    setActiveFilter,
+  } = useContext(AppContext);
+
+  const handleFilter = ({ target }) => {
+    if (activeFilter === '') {
+      setActiveFilter(target.value);
+    } else if (activeFilter === target.value) {
+      setActiveFilter('');
+    } else {
+      setActiveFilter(target.value);
+    }
+  };
+
+  const renderizeFiltered = async () => {
+    if (activeFilter === '') {
+      if (type === 'foods') {
+        const foodsResult = await fetchFoods();
+        setFoods(foodsResult);
+      } else {
+        const drinksResult = await fetchDrinks();
+        setDrinks(drinksResult);
+      }
+    }
+    if (activeFilter !== '' && activeFilter !== 'redirected') {
+      const fetchResult = await fetchFilteredByNationality(activeFilter);
+      setFoods(fetchResult);
+    }
+  };
+
+  useEffect(() => {
+    renderizeFiltered();
+  }, [activeFilter]);
+
+  const getNationalities = async () => {
+    const fetchResult = await fetchNationalities();
+    setNationalities(fetchResult);
+  };
+
+  useEffect(() => {
+    getNationalities();
+  }, []);
+
+  return (
+    <div>
+      <select
+        data-testid="explore-by-nationality-dropdown"
+        onChange={ handleFilter }
+      >
+        <option>
+          Select a nationality
+        </option>
+        <option
+          data-testid="All-option"
+        >
+          All
+        </option>
+        {
+          nationalities.map((e, i) => (
+            <option
+              key={ i }
+              data-testid={ `${e.strArea}-option` }
+              value={ e.strArea }
+            >
+              { e.strArea }
+            </option>
+          ))
+        }
+      </select>
+    </div>
+  );
+}
+
+NationalitiesDropDown.propTypes = {
+  type: PropTypes.string.isRequired,
+};
